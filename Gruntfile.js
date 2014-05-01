d31faf10e41551c86661c11b24edf08f7364438c
m90 <frederik.ring@gmail.com>
2014-05-01 20:20:57 +0200

    use static site generation for demos

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    assemble: {
+      options: {
+        assets: 'img',
+        layout: ['layouts/default.hbs'],
+        postprocess : require('pretty')
+      },
+      pages: {
+        src: ['*/*.hbs', '!layouts/**'],
+        dest: './'
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('assemble');
+
+  grunt.registerTask('default', ['assemble']);
+
+};
