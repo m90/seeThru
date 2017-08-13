e0d7bd64b5a0c1d761575336a216ef5fa260ec76
Frederik Ring <frederik.ring@gmail.com>
2017-08-13 19:26:24 +0200

    consider ready state when determining need for redraw

--- src/seeThru.js
+++ src/seeThru.js
@@ -249,11 +249,15 @@
 		, requestAnimationFrame = window.requestAnimationFrame || getRequestAnimationFrame()
 		, cancelAnimationFrame = window.cancelAnimationFrame || getCancelAnimationFrame()
 		, lastDrawnFrameTime = null
+		, lastDrawnReadyState = null
 		, drawFrame = function(recurse){
-			var image, alphaData, i, len, currentFrameTime = video.currentTime;
+			var image, alphaData, i, len
+				, currentFrameTime = video.currentTime
+				, currentReadyState = video.readyState;
 			
-			if (lastDrawnFrameTime !== currentFrameTime) {
+			if (lastDrawnFrameTime !== currentFrameTime || lastDrawnReadyState !== video.readyState){
 				lastDrawnFrameTime = currentFrameTime;
+				lastDrawnReadyState = currentReadyState;
 
 				buffer.drawImage(video, 0, 0, dimensions.width, dimensions.height * divisor); //scales if <video>-dimensions are not matching
 				image = buffer.getImageData(0, 0, dimensions.width, dimensions.height);
