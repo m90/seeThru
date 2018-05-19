ae4702262e71344739498cec26cd376867073a5d
Frederik Ring <frederik.ring@gmail.com>
2018-05-19 12:31:14 +0200

    allow passing of overrides for ff-family executables

--- bin/converter.js
+++ bin/converter.js
@@ -8,20 +8,41 @@ var FFmpeg = require('fluent-ffmpeg');
 var fs = require('fs');
 
 var argv = require('yargs')
-	.alias('i', 'in')
-	.alias('o', 'out')
+	.option('in', {
+		alias: 'i',
+		describe: 'The file to convert'
+	})
+	.option('out', {
+		alias: 'o',
+		describe: 'Where to write the conversion result'
+	})
+	.option('ffmpeg-path', {
+		describe: 'Location ffmpeg executable to use'
+	})
+	.option('ffprobe-path', {
+		describe: 'Location ffprobe executable to use'
+	})
 	.usage('Usage: $0 --in [originalfile] --out [convertedfile]')
-	.demand(['in', 'out'])
+	.demandOption(['in', 'out'])
 	.argv;
 
 var src = argv.in;
+
+if (argv.ffmpegPath) {
+	FFmpeg.setFfmpegPath(argv.ffmpegPath);
+}
+
+if (argv.ffprobePath) {
+	FFmpeg.setFfprobePath(argv.ffprobePath);
+}
 
 new FFmpeg({ source: src }).ffprobe(function (err, metadata) {
 
 	if (err) {
 		throw err;
 	} else if (!metadata) {
-		throw new Error('Video contains no metadata!');
+		console.error('Failed reading metadata from video');
+		process.exit(1);
 	}
 
 	var fileExt = src.split('.')[src.split('.').length - 1];
@@ -31,8 +52,8 @@ new FFmpeg({ source: src }).ffprobe(function (err, metadata) {
 
 	/* jshint multistr: true */
 	alpha.addOption(
-		'-vf'
-		, '[in] format=rgba,\
+		'-vf',
+		'[in] format=rgba,\
 		split [T1], fifo, lutrgb=r=maxval:g=maxval:b=maxval,\
 		[T2] overlay [out];\
 		[T1] fifo, lutrgb=r=minval:g=minval:b=minval [T2]'
@@ -40,14 +61,14 @@ new FFmpeg({ source: src }).ffprobe(function (err, metadata) {
 
 	alpha.on('error', function (err) {
 		console.error('An error occurred generating the alpha channel: ' + err.message);
-		throw err;
+		process.exit(1);
 	}).on('end', function () {
 
 		var rgb = new FFmpeg({ source: src });
 
 		rgb.addOption(
-			'-vf'
-			, '[in] scale=iw:ih,\
+			'-vf',
+			'[in] scale=iw:ih,\
 			pad=iw:2*ih [top];\
 			movie=seethru-tmp-alpha.mov,\
 			scale=iw:ih [bottom];\
@@ -64,7 +85,7 @@ new FFmpeg({ source: src }).ffprobe(function (err, metadata) {
 			});
 		}).on('error', function (err) {
 			console.error('An error occurred combining the video sources: ' + err.message);
-			throw err;
+			process.exit(1);
 		}).saveToFile(argv.out);
 
 	}).saveToFile('seethru-tmp-alpha.' + intermediateFormat);
